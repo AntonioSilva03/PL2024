3ac2d45529204ac6e6b225aef7f94ec4f9d97346
AntonioSilva03 <antoniofcsilva2003@gmail.com>
2024-02-15 21:56:00 +0000

    Finalização do TPC1 e escrita do README

--- TP1/README.md
+++ TP1/README.md
@@ -1,7 +1,24 @@
-# TPC1:
+<h1 align="center"> TPC1</h1>
 
 ## Autor:
 - António Filipe Castro Silva
 - A100533
 
-## Resumo:
+## Objetivo:
+
+O objetivo deste TPC é, a partir de um ficheiro CSV disponibilizado pelos professores com informações acerca de 300 atletas, sem utilizar o módulo CSV, obter os seguintes dados:
+- Lista ordenada alfabeticamente das modalidades desportivas
+- Percentagens de atletas aptos e inaptos para a prática desportiva
+- Distribuição de atletas por escalão etário (escalão = intervalo de 5 anos)
+
+## Resolução:
+
+Para resolver este TPC, formulei a resolução em 4 partes:
+
+- Inicialmente, trato de ler o CSV e colocar os dados todos processados num array e depois crio o ficheiro ([resultados.txt](https://github.com/AntonioSilva03/PL2024/blob/main/TP1/resultados.txt)) onde irei escrever os resultados requisitados.
+- A primeira query pede para ordenar as modalidades, logo comecei por colocar num array todas as modalidades existentes, somente na primeira vez que apareciam num
+dado atleta, depois ordenei-as alfabeticamente e, por fim, escrevias no ficheiro enumeradamente.
+- A segunda query pede a percentagem de atletas aptos e inaptos, logo primeiro calculei quantos havia de cada e depois calculei as percentagens. Depois dos cálculos escrevi no ficheiro o número de atletas aptos e inaptos e as suas percentagens correspondentes.
+- Por fim, na terceira query, comecei por calcular a maior idade e a menor idade entre os atletas, para escolher da melhor forma os intervalos dos escalões. Seguidamente, coloquei cada atleta no seu respetivo escalão e ordenei o escalão por idade e seguidamente por ordem alfabética. Finalmente escrevi no ficheiro o nome dos atletas que estão em cada escalão, tal como o número de atletas e a sua percentagem por escalão.
+
+Para finalizar fechei o ficheiro de resultados para melhor organização.
--- TP1/main.py
+++ TP1/main.py
@@ -11,50 +11,60 @@ for linha in dados.split("\n")[1:]:
     linha_processada = linha.split(",")
     dados_array.append(linha_processada)
 
+#Criação do ficheiro onde escreverei os resultados
+
 resultados = open("resultados.txt", "w", encoding="utf-8")
 
 #Modalidades
 
 modalidades = []
 
+#Colocar todas as modalidades existentes num array
 for linha in dados_array:
     modalidade = linha[8]
     if modalidade not in modalidades:
         modalidades.append(modalidade)
-modalidades.sort()
-resultados.write("Lista ordenada alfabeticamente das modalidades desportivas: " + "\n")
+modalidades.sort() #Organizar alfabeticamente
+resultados.write("Lista ordenada alfabeticamente das modalidades desportivas: " + "\n\n")
 i = 1
 for modalidade in modalidades:
-    resultados.write(str(i) + ". " + modalidade + "\n")
+    resultados.write(str(i) + ". " + modalidade + "\n") #Escrita no ficheiro
     i = i + 1
-resultados.write("\n")
+
+resultados.write("__________________________________________________________________\n")
 
 #Atletas aptos e inaptos
     
 atletas = len(dados_array)
 aptos = 0
 inaptos = 0
+#Cálculo do número de aptos e inaptos
 for linha in dados_array:
     if linha[12] == "true":
         aptos += 1
     else:
         inaptos += 1
 
+#Cálculo das percentagens e arredondamentos
 percentagem_aptos = (aptos / atletas) * 100
 percentagem_inaptos = 100 - percentagem_aptos
 aptos_arredondar = round(percentagem_aptos, 2)
 inaptos_arredondar = round(percentagem_inaptos, 2)
 
-resultados.write("Percentagens de atletas aptos e inaptos para a prática desportiva: " + "\n")
+#Escrita no ficheiro
+resultados.write("Percentagens de atletas aptos e inaptos para a prática desportiva: " + "\n\n")
 resultados.write("Atletas aptos: " + str(aptos) + " alunos-> " + str(aptos_arredondar) + "%" + "\n")
-resultados.write("Atletas inaptos: " + str(inaptos) + " alunos-> " + str(inaptos_arredondar) + "%" + "\n\n")
+resultados.write("Atletas inaptos: " + str(inaptos) + " alunos-> " + str(inaptos_arredondar) + "%" + "\n")
+
+resultados.write("___________________________________________________________________________\n")
 
 #Escalão Etário
 
 escaloes = {}
-min_idade = 100
-max_idade = 0
+min_idade = 1000
+max_idade = -1
 
+#Cálculo da menor e maior idade
 for linha in dados_array:
     idade = int(linha[5])
     if idade > max_idade:
@@ -62,12 +72,14 @@ for linha in dados_array:
     if idade < min_idade:
         min_idade = idade
 
+#Organização dos escalões
 i = min_idade
 while i <= max_idade:
     escalao = str(i) + "-" + str(i+ 4)
     escaloes[escalao] = None
     i = i + 5
 
+#Colocação de cada atleta no seu escalão etário
 for linha in dados_array:
     idade = int(linha[5])
     nome = linha[3] + " " + linha[4] + ", Idade: " + linha[5]
@@ -75,12 +87,25 @@ for linha in dados_array:
         min_idade, max_idade = map(int, escalao.split("-"))
         if min_idade<=idade<=max_idade:
             if escaloes[escalao] is None:
-                escaloes[escalao] = [nome]
+                escaloes[escalao] = [(nome,idade)]
             else:
-                escaloes[escalao].append(nome)
+                escaloes[escalao].append((nome,idade))
 
-resultados.write("Distribuição de atletas por escalão etário (escalão = intervalo de 5 anos): \n")
+#Sort dos atletas dentro do seu escalao primeiro pela idade e depois pelo nome
 for escalao, atletas in escaloes.items():
-    resultados.write("Escalão " + escalao +": " + str(atletas) + "\n")
+    escaloes[escalao] = sorted(atletas, key=lambda x: (x[1], x[0]))
+
+#Escrita no ficheiro
+resultados.write("Distribuição de atletas por escalão etário (escalão = intervalo de 5 anos): \n\n")
+for escalao, atletas in escaloes.items():
+    num_atletas = len(dados_array)
+    atletas_escalao = len(atletas)
+    last_escalao = list(escaloes.keys())[-1]
+    percentagem_escalao = (atletas_escalao / num_atletas) * 100
+    resultado_escalao = "\n".join([atleta[0] for atleta in atletas])
+    if escalao != last_escalao:
+        resultados.write(f"Escalão {escalao} ({atletas_escalao } atletas, {percentagem_escalao:.2f}%):\n\n{resultado_escalao}\n\n")
+    else:
+        resultados.write(f"Escalão {escalao} ({atletas_escalao } atletas, {percentagem_escalao:.2f}%):\n\n{resultado_escalao}")
 
 resultados.close()       
